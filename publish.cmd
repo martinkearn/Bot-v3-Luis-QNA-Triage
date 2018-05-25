b7a7e3a27f5127edd70da231511bc278d611a94c
Martin Kearn <martin.kearn@microsoft.com>
2018-05-25 12:00:22 +0100

    Initial templated bot service project

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=luisqnatriage-Web-Deploy.pubxml -p:Password=rQ9qitykreNpJ3Fo5Jww0oxzeaHNty6E3CqdwWZuxJSNPLNLmXQxMKyCMqvM
+
