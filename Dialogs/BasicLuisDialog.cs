d47bc08df234be0b0450e66b8e1f54a9c7d17e72
Martin Kearn <martin.kearn@microsoft.com>
2018-05-29 15:51:54 +0100

    Minor copy update to test CI

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -47,7 +47,7 @@ namespace Microsoft.Bot.Sample.LuisBot
 
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
         {
-            await context.PostAsync($"You have arrived at {result.Intents[0].Intent}. You said: {result.Query}");
+            await context.PostAsync($"You seem to have arrived at {result.Intents[0].Intent}. You said: {result.Query}");
             context.Wait(MessageReceived);
         }
     }
