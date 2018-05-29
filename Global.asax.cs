b8275afeeace9a2f3e141dcec8ea8e37278c221f
Martin Kearn <martin.kearn@microsoft.com>
2018-05-29 16:10:56 +0100

    Copy and CSPROJ change

--- Global.asax.cs
+++ Global.asax.cs
@@ -25,11 +25,11 @@ namespace SimpleEchoBot
                     builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
 
                     // Using Azure Table Storage
-                    var store = new TableBotDataStore(ConfigurationManager.AppSettings["AzureWebJobsStorage"]); // requires Microsoft.BotBuilder.Azure Nuget package 
+                    //var store = new TableBotDataStore(ConfigurationManager.AppSettings["AzureWebJobsStorage"]); // requires Microsoft.BotBuilder.Azure Nuget package 
 
                     // To use CosmosDb or InMemory storage instead of the default table storage, uncomment the corresponding line below
                     // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
-                    // var store = new InMemoryDataStore(); // volatile in-memory store
+                    var store = new InMemoryDataStore(); // volatile in-memory store
 
                     builder.Register(c => store)
                         .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
